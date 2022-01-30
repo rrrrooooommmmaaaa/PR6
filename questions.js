c09385e25609c725618c1630162b235e1342e317
rrrrooooommmmaaaa <93996455+rrrrooooommmmaaaa@users.noreply.github.com>
2022-01-30 21:25:00 +0200

    PR 6.2

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-const questions = [
+export const questions = [
     {
         question: "Какого цвета бургер?",
         answers: [
@@ -73,24 +73,24 @@ const questions = [
     }
 ];
 
-const obj = {}
+// const obj = {}
 
-const getData = () => {
-    formAnswers.textContent = 'LOAD';
+// const getData = () => {
+//     formAnswers.textContent = 'LOAD';
 
-    setTimeout(() => {
-        fetch('http://localhost:81/Quiz__intens/db.json')
-            .then(res => res.json())
-            .then(obj => playTest(obj.questions))
-    }, 2000);
-}
+//     setTimeout(() => {
+//         fetch('http://localhost:81/Quiz__intens/db.json')
+//             .then(res => res.json())
+//             .then(obj => playTest(obj.questions))
+//     }, 2000);
+// }
 
-const obj = {};
+// const obj = {};
 
-const inputs = [...formAnswers.elements]
-    .filter(elem => elem.checked)
+// const inputs = [...formAnswers.elements]
+//     .filter(elem => elem.checked)
 
-inputs.forEach((elem, index) => {
-    obj[`${index}_${questions[numberQuestion].question}`] = elem.value;
-})
-finalAnswers.push(obj)
+// inputs.forEach((elem, index) => {
+//     obj[`${index}_${questions[numberQuestion].question}`] = elem.value;
+// })
+// finalAnswers.push(obj)
--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
-const BURGER_SRC = './image/burger.png'
-const BURGER_TITLE = 'Стандарт'
+import { questions } from '../questions.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     const btnOpenModal = document.querySelector('#btnOpenModal')
@@ -7,6 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeModal = document.querySelector('#closeModal')
     const questionTitle = document.querySelector('#question')
     const formAnswers = document.querySelector('#formAnswers')
+    const nextButton = document.querySelector('#next')
+    const prevButton = document.querySelector('#prev')
 
     btnOpenModal.addEventListener('click', () => {
         modalBlock.classList.add('d-block')
@@ -18,19 +19,45 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     const playTest = () => {
-        const renderQuestions = () => {
-            questionTitle.textContent = 'Какого цвета бургер вы хотите?';
+        let numberQuestion = 0
 
-            formAnswers.innerHTML = `
-                <div class="answers-item d-flex flex-column" >
-                    <input type="radio" id="answerItem1" name="answer" class="d-none">
-                    <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                        <img class="answerImg" src=${BURGER_SRC} alt="burger">
-                        <span>${BURGER_TITLE}</span>
-                    </label>
-                </div>
-            `
+        const createAnswer = ({ title, url, type }) => `
+            <div class="answers-item d-flex flex-column" >
+                <input type=${type} id=${title} name="answer" class="d-none">
+                <label for=${title} class="d-flex flex-column justify-content-between">
+                    <img class="answerImg" src=${url} alt="burger">
+                    <span>${title}</span>
+                </label>
+            </div>
+        `
+
+        const renderAnswers = (number) => {
+            questions[number].answers.forEach((answer, index) => {
+                formAnswers.insertAdjacentHTML('beforeend', createAnswer({ ...answer, type: questions[number].type }))
+            })
         }
-        renderQuestions();
+
+        const renderQuestions = (number) => {
+            formAnswers.innerHTML = ''
+
+            prevButton.style.display = numberQuestion === 0 ? 'none' : 'block'
+            nextButton.style.display = numberQuestion === questions.length - 1 ? 'none' : 'block'
+
+            questionTitle.textContent = questions[number].question;
+
+            renderAnswers(number)
+        }
+
+        renderQuestions(numberQuestion);
+
+        nextButton.onclick = () => {
+            numberQuestion++;
+            renderQuestions(numberQuestion)
+        }
+
+        prevButton.onclick = () => {
+            numberQuestion--;
+            renderQuestions(numberQuestion)
+        }
     }
 })
