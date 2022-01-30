ddb7ae61726defbbcd4673500a5f3a90cf589e81
rrrrooooommmmaaaa <93996455+rrrrooooommmmaaaa@users.noreply.github.com>
2022-01-30 20:49:57 +0200

    PR 6.1

---
+++ script/script.js
@@ -0,0 +1,36 @@
+const BURGER_SRC = './image/burger.png'
+const BURGER_TITLE = 'Стандарт'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btnOpenModal = document.querySelector('#btnOpenModal')
+    const modalBlock = document.querySelector('#modalBlock');
+    const closeModal = document.querySelector('#closeModal')
+    const questionTitle = document.querySelector('#question')
+    const formAnswers = document.querySelector('#formAnswers')
+
+    btnOpenModal.addEventListener('click', () => {
+        modalBlock.classList.add('d-block')
+        playTest();
+    })
+
+    closeModal.addEventListener('click', () => {
+        modalBlock.classList.remove('d-block')
+    })
+
+    const playTest = () => {
+        const renderQuestions = () => {
+            questionTitle.textContent = 'Какого цвета бургер вы хотите?';
+
+            formAnswers.innerHTML = `
+                <div class="answers-item d-flex flex-column" >
+                    <input type="radio" id="answerItem1" name="answer" class="d-none">
+                    <label for="answerItem1" class="d-flex flex-column justify-content-between">
+                        <img class="answerImg" src=${BURGER_SRC} alt="burger">
+                        <span>${BURGER_TITLE}</span>
+                    </label>
+                </div>
+            `
+        }
+        renderQuestions();
+    }
+})
